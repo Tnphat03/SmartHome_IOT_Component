30e3e464cab9f189c3c985867517bb2237d37288
ELephant47 <vandinhdung2003@gmail.com>
2024-12-15 08:54:22 +0700

    #update all

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import FlameSensor from './components/FlameSensor' // Import the FlameSensor component
-import RoomCard from './components/RoomCard' 
-const App: React.FC = () => {
-  return (
-    <div className="flex justify-center items-center h-screen space-x-8">
-      {/* Render three FlameSensor components */}
-      <FlameSensor />
-  
-      <RoomCard name={''} status={''} price={0} />
-    </div>
-  );
-};
+import './App.css'
+// import { Toaster } from 'react-hot-toast';
+import { createBrowserRouter, Navigate, Outlet, Route, RouteObject, RouterProvider, Routes,useLocation  } from 'react-router-dom';
+import { publicRoutes } from './routes/Routes';
+import { useTheme } from './context/themeContext';
+import MainLayout from './layouts/MainLayout';
 
-export default App;
+export enum Roles {
+  ADMIN,
+  STUDENT,
+}
+
+function App() {
+//   const [count, setCount] = useState(0)
+  const { theme } = useTheme();
+  const role = Roles.ADMIN
+  return (
+    <>
+       {/* <Toaster
+          position="top-center"
+          containerStyle={{
+             zIndex: 1000000000000,
+          }}
+          toastOptions={{
+             style: {
+                background: theme ? 'rgba(0,0,0,0.5)' : 'rgba(0,0,0,0.5)',
+                color: theme ? 'white' : 'black',
+                backdropFilter: 'blur(20rem)',
+                userSelect: 'none',
+             },
+          }}
+       /> */}
+       <Routes>
+          {/* Public Routes */}
+          {publicRoutes.map((route, index) => (
+             <Route
+                key={index}
+                path={route.path}
+                element={<MainLayout >{route.component}</MainLayout>}
+             />
+          ))}
+       </Routes>
+    </>
+ );
+}
+
+export default App
